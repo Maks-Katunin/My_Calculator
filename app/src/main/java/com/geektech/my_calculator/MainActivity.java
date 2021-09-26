b4945ad671744b5902985be942b5c35cdf782bdc
Maks-Katunin <katunin87@gmail.com>
2021-09-27 01:27:09 +0600

    add operation plus add id for button added logic to buttons now everything works almost correctly.

--- app/src/main/java/com/geektech/my_calculator/MainActivity.java
+++ app/src/main/java/com/geektech/my_calculator/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvResult;
     private Integer firstValue, secondValue;
     private String operation = "";
-    private Boolean tv_result_is_EMPTY = true;
+    private Boolean finish = false;
     private String result;
     private String secondValueString;
 
@@ -26,82 +26,91 @@ public class MainActivity extends AppCompatActivity {
 
     public void onNumberClick(View view) {
         switch (view.getId()) {
-
-
             case R.id.button_1:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("1");
+                    finish = false;
                 } else {
                     tvResult.append("1");
+
                 }
                 break;
             case R.id.button_2:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("2");
+                    finish = false;
                 } else {
                     tvResult.append("2");
                 }
                 break;
 
             case R.id.button_3:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("3");
+                    finish = false;
                 } else {
                     tvResult.append("3");
                 }
                 break;
 
             case R.id.button_4:
-                if (tvResult.getText().toString().equals("0")) {
-                    tvResult.setText("5");
+                if (tvResult.getText().toString().equals("0") || finish) {
+                    tvResult.setText("4");
+                    finish = false;
                 } else {
                     tvResult.append("4");
                 }
                 break;
 
             case R.id.button_5:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("5");
+                    finish = false;
                 } else {
                     tvResult.append("5");
                 }
                 break;
 
             case R.id.button_6:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("6");
+                    finish = false;
                 } else {
                     tvResult.append("6");
                 }
                 break;
 
             case R.id.button_7:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("7");
+                    finish = false;
                 } else {
                     tvResult.append("7");
                 }
                 break;
 
             case R.id.button_8:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("8");
+                    finish = false;
                 } else {
                     tvResult.append("8");
                 }
                 break;
 
             case R.id.button_9:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("9");
+                    finish = false;
                 } else {
                     tvResult.append("9");
                 }
                 break;
 
             case R.id.button_zero:
-                if (tvResult.getText().toString().equals("0")) {
+                if (tvResult.getText().toString().equals("0") || finish) {
                     tvResult.setText("0");
+                    finish = false;
                 } else {
                     tvResult.append("0");
                 }
@@ -114,78 +123,85 @@ public class MainActivity extends AppCompatActivity {
     public void onOperationClick(View view) {
         switch (view.getId()) {
             case R.id.button_plus:
-                firstValue = Integer.parseInt(tvResult.getText().toString());
-                operation = "+";
-                tvResult.setText(firstValue + operation);
+                if (!tvResult.getText().toString().contains("+")) {
+                    firstValue = Integer.parseInt(tvResult.getText().toString());
+                    operation = "+";
+                    tvResult.setText(firstValue + operation);
+                }
                 break;
 
             case R.id.button_minus:
-                firstValue = Integer.parseInt(tvResult.getText().toString());
-                operation = "-";
-                tvResult.setText(firstValue + operation);
+                if (!tvResult.getText().toString().contains("-")) {
+                    firstValue = Integer.parseInt(tvResult.getText().toString());
+                    operation = "-";
+                    tvResult.setText(firstValue + operation);
+                }
                 break;
 
             case R.id.button_Division_of_numbers:
-                firstValue = Integer.parseInt(tvResult.getText().toString());
-                operation = "÷";
-                tvResult.setText(firstValue + operation);
+                if (!tvResult.getText().toString().contains("÷")) {
+                    firstValue = Integer.parseInt(tvResult.getText().toString());
+                    operation = "÷";
+                    tvResult.setText(firstValue + operation);
+                }
                 break;
 
             case R.id.button_multiplication:
-                firstValue = Integer.parseInt(tvResult.getText().toString());
-                operation = "×";
-                tvResult.setText(firstValue + operation);
+                if (!tvResult.getText().toString().contains("×")) {
+                    firstValue = Integer.parseInt(tvResult.getText().toString());
+                    operation = "×";
+                    tvResult.setText(firstValue + operation);
+                }
                 break;
 
 
             case R.id.button_equals:
-                String two = tvResult.getText().toString().replace(firstValue + operation, "");
-                secondValue = Integer.parseInt(two);
-                String result = String.valueOf(firstValue + secondValue);
-                tvResult.setText(firstValue + operation + two + "=" + result);
+                if (!tvResult.getText().toString().contains("=")) {
+                    String two = tvResult.getText().toString().replace(firstValue + operation, "");
+                    secondValue = Integer.parseInt(two);
+                    String result = String.valueOf(firstValue + secondValue);
+                    tvResult.setText(result);
+                }
                 break;
         }
     }
 
     public void computeCalculation(View view) {
+        finish = true;
 
         switch (operation) {
-            case "":
-                tvResult.setText("0");
-                break;
-
-
             case "+":
                 secondValueString = tvResult.getText().toString().replace(firstValue + operation, "");
                 secondValue = Integer.parseInt(secondValueString);
                 result = String.valueOf(firstValue + secondValue);
-                tvResult.setText(firstValue + operation + secondValueString + "=" + result);
+                tvResult.setText(firstValue + operation + secondValue + "=" + result);
                 operation = "";
                 break;
             case "-":
                 secondValueString = tvResult.getText().toString().replace(firstValue + operation, "");
                 secondValue = Integer.parseInt(secondValueString);
                 result = String.valueOf(firstValue - secondValue);
-                tvResult.setText(firstValue + operation + secondValueString + "=" + result);
+                tvResult.setText(firstValue + operation + secondValue + "=" + result);
                 operation = "";
                 break;
             case "×":
                 secondValueString = tvResult.getText().toString().replace(firstValue + operation, "");
                 secondValue = Integer.parseInt(secondValueString);
                 result = String.valueOf(firstValue * secondValue);
-                tvResult.setText(firstValue + operation + secondValueString + "=" + result);
+                tvResult.setText(firstValue + operation + secondValue + "=" + result);
                 operation = "";
                 break;
             case "÷":
                 secondValueString = tvResult.getText().toString().replace(firstValue + operation, "");
                 secondValue = Integer.parseInt(secondValueString);
                 result = String.valueOf(firstValue / secondValue);
-                tvResult.setText(firstValue + operation + secondValueString + "=" + result);
+                tvResult.setText(firstValue + operation + secondValue + "=" + result);
                 operation = "";
                 break;
 
             default:
                 tvResult.setText("0");
+                break;
         }
 
     }
